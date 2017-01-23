81f0ce8970f73039f53e9827ef14165a8c9bf73c
zhangyuanyuan031 <zhangyuanyuan031@pingan.com.cn>
2017-01-23 17:59:51 +0800

    init project

---
+++ test/com/yuan/test/DemoTest.java
@@ -0,0 +1,13 @@
+package com.yuan.test;
+
+import org.junit.Test;
+
+/**
+ * Created by zhangyuanyuan031 on 2017/1/23.
+ */
+public class DemoTest {
+    @Test
+    public void testDemo() throws Exception{
+        System.err.println("hello word");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,13 @@
+<%--
+  Created by zhangyuanyuan031 on 2017/1/19
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>mytitle</title>
+  </head>
+  <body>
+      aaaabbb
+  dddddbbb
+  </body>
+</html>
